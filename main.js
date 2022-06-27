2728c53fedc657397a47e71c6f817622bba94083
riley soloner <rileysoloner@gmail.com>
2022-06-27 17:32:58 -0400

    updated javascript, css and html

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 // alert('JavaScript is working');
+
+const pokemon = ['charmander', 'bulbasaur', 'squirtle']
+
+pokemonIDs = ['001', '004', '007']
+// DOM = Document Object Model
+//  loop over every ID
+// create an html element
+// set the html element values
+// append HTML element to the DOM
+
+pokemonIDs.map((id) => {
+    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`
+    console.log('img Url', imgUrl)
+    let div = document.createElement('div') 
+    div.setAttribute('class', 'pokemon-card') 
+    let img = document.createElement('img') 
+    img.src = imgUrl
+    img.setAttribute('height', '100px')
+    img.setAttribute('width', '100px')
+    div.append(img)
+    document.body.append(div)
+})
