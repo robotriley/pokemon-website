b74ae4ad0d11825a6e210d39f3b6e238875ab9c8
riley soloner <rileysoloner@gmail.com>
2022-06-28 12:06:07 -0400

    added button in html and audio functions in Js

--- main.js
+++ main.js
@@ -6,18 +6,34 @@ const pokemon = [
     {name: 'Charmander', id: '004'},
     {name: 'Squirtle', id: '007'},
 ]
-const containerDiv = document.querySelector('#container')
 
+const containerDiv = document.querySelector('#container')
+const newBtn = document.querySelector('#new-pokemon-btn')
+
+newBtn.addEventListener('click', () => {
+    let num = prompt('ENTER A POKEMON NUMBER')
+    console.log('Number entered', num)
+})
 pokemon.map((element, index) => {
-    console.log(element)
+    console.log(element) //  loop over every element
     let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${element.id}.png`
     let div = document.createElement('div') 
     let h3 = document.createElement('h3')
     h3.innerText = element.name
     div.setAttribute('class', 'pokemon-card') 
     let img = document.createElement('img') 
+    let audioUrl = `https://play.pokemonshowdown.com/audio/cries/${element.name.toLowerCase()}.mp3`
+    let audio = document.createElement('audio')
+    let source = document.createElement('source')
+    source.setAttribute('src', audioUrl)
+    source.setAttribute('type', 'audio/mpeg')
+    audio.append(source)
+    div.addEventListener('click',() => {
+        console.log('audio', audioUrl)
+        audio.play()
+    })
     img.src = imgUrl    
-    div.append(img, h3)
+    div.append(img, h3, audio)
     containerDiv.append(div)
 })
 
