dfe4a10b0a498a80a060abd10fbde370d095211f
riley soloner <rileysoloner@gmail.com>
2022-06-28 13:51:13 -0400

    added roster

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ const pokemon = [
     {name: 'Bulbasaur', id: '001'},
     {name: 'Charmander', id: '004'},
     {name: 'Squirtle', id: '007'},
+
 ]
+const userRoster = []
 
 const containerDiv = document.querySelector('#container')
 const newBtn = document.querySelector('#new-pokemon-btn')
@@ -14,7 +16,15 @@ const rosterDiv = document.querySelector('#roster')
 newBtn.addEventListener('click', () => {
     let num = prompt('ENTER A POKEMON NUMBER')
     console.log('Number entered', num)
+    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${num}.png`
+    let img = document.createElement("img")
+    img.setAttribute('src', imgUrl)
+    img.setAttribute('class', 'roster-img')
+    let position = document.querySelector(`#pokemon-${userRoster.length + 1}`)
+    position.append(img)
+    userRoster.push(num)
 })
+
 pokemon.map((element, index) => {
     console.log(element) //  loop over every element
     let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${element.id}.png`
